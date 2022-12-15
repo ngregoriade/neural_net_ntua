222f3212cde7915b45395e8cd5cf8a4980fae773
Natalia Grigoriadou <el18940@mail.ntua.gr>
2022-12-15 21:14:44 +0200

    changes in the formation and initial for 2nd part

--- tmp/joblib/imblearn/pipeline/_fit_resample_one/func_code.py
+++
@@ -1,6 +0,0 @@
-# first line: 392
-def _fit_resample_one(sampler, X, y, message_clsname="", message=None, **fit_params):
-    with _print_elapsed_time(message_clsname, message):
-        X_res, y_res = sampler.fit_resample(X, y, **fit_params)
-
-        return X_res, y_res, sampler
